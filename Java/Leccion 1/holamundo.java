158897db63973e78f1ce5da412f8419b1586a98d
Diego Alvarez <Diegooalvarez@hotmail.com>
2022-05-04 14:20:21 -0300

    agregando java y py

--- Java/Leccion 1/holamundo.java
+++ Java/Leccion 1/holamundo.java
@@ -1,5 +1,30 @@
+import javax.print.attribute.SupportedValuesAttribute;
+
 public class holamundo {
     public static void main(String[] args) {
-        System.out.print("Hola mundo desde java ");
+        System.out.println("Hola mundo desde java ");
+        // De tipo entero
+        int myVariable = 10;
+        System.out.println(myVariable);
+        myVariable = 5;
+        System.out.println(myVariable);
+        // De tipo string
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = " Bueno a sido un placer conocerte hasta luego";
+        System.out.println(miVariableCadena);
+
+        // var - inferencia de tipos
+        var myVariable2 = 44;
+        var miVariableCadena2 = "Seguimos estudiando ";
+        System.out.println(myVariable2 + " El mejor numero ");
+        System.out.println(miVariableCadena2 + "Vamos a seguir mejorando!!");
+        // soutv + tab te escribe el codigo
+
+        var nombre = "Diego";
+        var titulo = "Estudiante Tecnico en Programacion";
+        var union = nombre + " " + titulo;
+        System.out.println(union);
+
     }
-} 
+}
---
+++ Python/Lecion 2/ejercicio.py
@@ -0,0 +1,31 @@
+# Actividad de ancho largo
+"""
+ancho=int(input("Digite el ancho "))
+largo=int(input("Digite el largo "))
+
+area = (ancho*largo)
+perimetro = (largo + ancho)*2
+print("El area es : ")
+print(int(area))
+
+print("El perimetro es : ")
+print(int(perimetro))
+"""
+# Actividad para determinar el par o impar
+"""
+num=int(input("Digite un numero "))
+if num%2 == 0:
+    print(f"True")
+else:
+    print("False")
+
+"""
+# Actividad para validar si es mayor de edad
+
+edad=int(input(" Digite su edad: "))
+if edad==18:
+    print(f"Usted es mayor de edad raspando tiene {edad}")
+elif edad>18:
+    print(f"Usted tiene {edad} por lo tanto es mayor")
+elif edad<18:
+    print(f"Usted tiene {edad} por lo tanto es menor de edad no puede ingresar")
