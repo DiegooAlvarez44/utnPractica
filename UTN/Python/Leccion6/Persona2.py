e8cc90d86aae8d41de06646d3d1c3f928e7f834c
DiegoAlvarez44 <diegooalvarez@hotmail.com>
2022-10-27 16:27:52 +0200

    Leccion6 py terminado

--- UTN/Python/Leccion6/Persona2.py
+++ UTN/Python/Leccion6/Persona2.py
@@ -39,45 +39,50 @@ class Persona2:
     def edad(self, edad):
         self._edad = edad
     
+    def __del__(self):
+        print(f'Persona2: {self._nombre} {self._apellido} {self._edad} ')
+    
+if __name__ == '__main__':
+    persona1 = Persona2('Diego', 'Alvarez', 24)    
+    #print(persona1._nombre) #Esto no se debe hacer
+    print(persona1.nombre) #Llamamos al metodo getter
+    persona1.nombre = 'Pedro Pompin' # Llmamos el metodo setter
+    print(persona1.nombre) # Otra vez al metodo getter
+    print(persona1.mostrar_detalles()) # LLamamos el metodo para mostra detalles
+    #Atributo read-only (Solo lectura)seria la edad por quie no tiene el metodo set
+    print(persona1.edad)
+    # Tarea crar tres objetos mas, utilizando los metodos getter and setter 
+    # para modificar, y mostrar los cambios
 
-persona1 = Persona2('Diego', 'Alvarez', 24)    
-#print(persona1._nombre) #Esto no se debe hacer
-print(persona1.nombre) #Llamamos al metodo getter
-persona1.nombre = 'Pedro Pompin' # Llmamos el metodo setter
-print(persona1.nombre) # Otra vez al metodo getter
-print(persona1.mostrar_detalles()) # LLamamos el metodo para mostra detalles
-#Atributo read-only (Solo lectura)seria la edad por quie no tiene el metodo set
-print(persona1.edad)
-# Tarea crar tres objetos mas, utilizando los metodos getter and setter 
-# para modificar, y mostrar los cambios
+    persona2 = Persona2('Meli', 'Arnola', 21)
+    print(persona2.nombre)
+    print(persona2.apellido)
+    print(persona2.edad)
+    persona2.nombre = 'Melisa'
+    persona2.apellido = 'Arnolita'
+    persona2.edad = 21
+    print(persona2.mostrar_detalles())
 
-persona2 = Persona2('Meli', 'Arnola', 21)
-print(persona2.nombre)
-print(persona2.apellido)
-print(persona2.edad)
-persona2.nombre = 'Melisa'
-persona2.apellido = 'Arnolita'
-persona2.edad = 21
-print(persona2.mostrar_detalles())
+    persona3 = Persona2('Carli', 'Fonseca', 23)
+    print(persona2.nombre)
+    print(persona2.apellido)
+    print(persona2.edad)
+    persona3.nombre = 'Carla'
+    persona3.apellido = 'Fonseka'
+    persona3.edad = 44
 
-persona3 = Persona2('Carli', 'Fonseca', 23)
-print(persona2.nombre)
-print(persona2.apellido)
-print(persona2.edad)
-persona3.nombre = 'Carla'
-persona3.apellido = 'Fonseka'
-persona3.edad = 44
+    print(persona3.mostrar_detalles())
 
-print(persona3.mostrar_detalles())
+    persona4 = Persona2('Leo', 'Cache', 30)
+    print(persona2.nombre)
+    print(persona2.apellido)
+    print(persona2.edad)
+    persona4.nombre = 'Leonel'
+    persona4.apellido = 'Cachefo'
+    persona4.edad = 25
+    print(persona4.mostrar_detalles())
 
-persona4 = Persona2('Leo', 'Cache', 30)
-print(persona2.nombre)
-print(persona2.apellido)
-print(persona2.edad)
-persona4.nombre = 'Leonel'
-persona4.apellido = 'Cachefo'
-persona4.edad = 25
-print(persona4.mostrar_detalles())
+    print(__name__)
 
 
 
--- UTN/Python/Leccion6/PruebaPersona2.py
+++ UTN/Python/Leccion6/PruebaPersona2.py
@@ -1,2 +1,12 @@
-from Persona2 import *
- 
+from typing_extensions import Self
+from Persona2 import Persona2
+
+
+print('Creacion de objetos'.center(50, '-'))
+if __name__ == '__main__':
+    Persona5 = Persona2('Lionel', 'Messi', 35)
+    Persona5.mostrar_detalles()
+    print(__name__)
+
+print('Eliminacion de Objetos'.center(50, '-'))
+del Persona5
